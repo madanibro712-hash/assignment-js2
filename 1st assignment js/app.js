16ff0486a770c71b71af85aa3797402e3e3c7e45
madaniraza140 <madanibro712@gmail.com>
2025-09-15 11:33:39 -0700

    hello js
    this is my javascript 1st assignment

---
+++ 1st assignment js/app.js
@@ -0,0 +1,123 @@
+//     // 2nd chapter ans
+// var name = "jhone doe"
+// alert(name)
+
+// var myname = "madani-raza"
+// alert(myname)
+
+// var title ="helloWorld "
+// alert(title)
+
+// var studentname ="alikhan"
+// var studentage ="18years"
+// var studentschool ="jawanpakistan"
+
+// alert (studentname)
+// alert (studentage)
+// alert (studentschool)
+ 
+
+// //   chapter 3 ans
+
+// let age = "i am 16y old"
+// alert (age)
+
+// let manytime = "you are visited to my web 14times "
+
+// document.write("my birth year is 2010 <br>")
+
+// let ame = prompt("Enter your name:");
+// let product = prompt("Enter product title:");
+// let quantity = ("Enter quantity:"); 
+
+// document.write(ame + " "+" ordered " + quantity + "5" + product + "(s) on XYZ Clothing store");
+ 
+// //   chapter 4 ans
+
+// //  declear one line 3 statement 
+// let a = 10, b = 20, c = 30;
+
+
+// //    legal variables name
+// var $ali ;
+// var name ;
+// var username ;
+// var fullname;
+// var  user_age;
+
+// //   illegal varibales
+
+// // var @madani;
+// // var 1stname
+// // var full-name;
+// // var  user*id ;
+// // var first name;
+
+// document .write (  "<br> Rules for naming js variables <br>")
+ 
+// document.write("Varibles names can only contain ,numbers,$ and for example,_my1ST Variable <br> variable must begin with a letter $ or _ $name _name or name <br> variable names cade are very sensitive  <br> Variables name should not be js keywords")
+
+
+//  chapter 5 answers
+//  var num = 10
+//  var num2 =3
+
+//  document.write("subscription"+":", (num-num2) + "<br>")
+//  document.write("addition"+ ":", (num+num2) + "<br>")
+//  document.write("multiplication"+":", (num*num2) + ":","<br>")
+//  document.write("division"+ ":", (num/num2) + "<br>")
+//  document.write("Modulas"+ ":", (num %num2) + "<br>")
+
+
+var  myvar;
+document.write("value after variable declearation ",myvar ,"<br>")
+
+myvar =5
+document.write("intiale value"+ " " +myvar + "<br>")
+
+myvar++;
+document.write("value after addition (increament)"+ " " +myvar + "<br>")
+
+myvar = myvar +7;
+document.write("new value after adding"+ myvar,"<br>")
+
+myvar--;
+document.write("value after subscription (decreament)"+ " " +myvar + "<br>")
+
+var remainder = myvar % 3;
+document.write("The remainder is: " + remainder + "<br>");
+
+
+var ticketPrice = 600;
+var totalprice = ticketPrice * 5;
+
+document.write("total cost to buy 5 ticket to a movie is"+" "+ totalprice+"PKR"+"<br>");
+
+// table of 4
+var tablenumber = 4;
+var range_number=10;
+
+for(var yz=1; yz <= range_number; yz++
+){
+document.write(tablenumber , "x",yz , "=",tablenumber * yz,"<br>")
+};
+
+var price_item_1 =1500
+var price_item_2 =1200
+
+
+var quantity_1 = 1
+var quantity_2 = 6
+
+
+var  totalcost = (price_item_1 * quantity_1) +(price_item_2*quantity_2) 
+
+document.write("<h2>Shopping Card</h2> <br>")
+document.write("price of item 1" , " ", price_item_1,"<br>")
+
+document.write("price of item 2" , " ", price_item_2,"<br>")
+
+document.write("quantity of item 1" , ":" ,quantity_1," ","<br>");
+document.write("quantity of item 2" , ":", quantity_2," ","<br>");
+
+document.write("Your total bill is" ," ",totalcost,"PKR")
